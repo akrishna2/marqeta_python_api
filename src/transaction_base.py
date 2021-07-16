12f4e049b3e826b2c56b8a6e1f400bf0de82fde5
akrishna@cccis.com <AKrishna@win.cccis.com>
2021-07-16 17:14:24 -0500

    Added allure reporting

--- src/transaction_base.py
+++ src/transaction_base.py
@@ -1,6 +1,28 @@
+import logging
+import allure
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+
+logger = logging.getLogger(__name__)
+logger.propagate = False
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+
+class AllureLoggingHandler(logging.Handler):
+    def log(self, message):
+        logger.info(message)
+        with allure.step(f'{message}'):
+            pass
+
+    def emit(self, record):
+        self.log("({}) {}".format(record.levelname, record.getMessage()))
+
+
+allure_handler = AllureLoggingHandler()
+allure_handler.setLevel(logging.DEBUG)
+
 
 class TransactionBase(object):
     def __init__(self):
@@ -8,6 +30,7 @@ class TransactionBase(object):
         self.card_product_token = None
         self.card_token = None
 
+    @allure.step("Get user token")
     def create_user(self):
         data = '{"first_name": "Abhishek","last_name": "Krishna","active": true}'
         user = "18da7496-be57-4d36-abb5-00f53dd8bb17"
@@ -22,18 +45,20 @@ class TransactionBase(object):
         self.user_token = response.json()['token']
         return self
 
+    @allure.step("Retrieve user using user token")
     def retrive_user(self):
         user = "18da7496-be57-4d36-abb5-00f53dd8bb17"
         password = "acda937f-6b95-4789-a1c8-613be4117c2b"
-        response = requests.get("https://sandbox-api.marqeta.com/v3/users/"+self.user_token,
-                                 headers={
-                                     'content-type': 'application/json',
-                                 },
-                                 auth=HTTPBasicAuth(user, password))
+        response = requests.get("https://sandbox-api.marqeta.com/v3/users/" + self.user_token,
+                                headers={
+                                    'content-type': 'application/json',
+                                },
+                                auth=HTTPBasicAuth(user, password))
         assert response.status_code == 200
         # print(json.dumps(response.json(), indent=4))
         return self
 
+    @allure.step("Get card product")
     def get_card_product(self):
         data = '{}'
         response = requests.get("https://sandbox-api.marqeta.com/v3/cardproducts",
@@ -47,6 +72,7 @@ class TransactionBase(object):
         self.card_product_token = response.json()['data'][0]['token']
         return self
 
+    @allure.step("Create Card using user token and card product token")
     def create_card(self):
         data = '{"user_token": "' + self.user_token + '" ,"card_product_token": "' + self.card_product_token + '"} '
         response = requests.post("https://sandbox-api.marqeta.com/v3/cards",
@@ -60,16 +86,18 @@ class TransactionBase(object):
         self.card_token = response.json()['token']
         return self
 
+    @allure.step("Retrieve card using card token")
     def retrieve_card(self):
-        response = requests.get("https://sandbox-api.marqeta.com/v3/cards/"+self.card_token,
-                                 headers={
-                                     'content-type': 'application/json',
-                                     'accept': 'application/json',
-                                     'Authorization': 'Basic MThkYTc0OTYtYmU1Ny00ZDM2LWFiYjUtMDBmNTNkZDhiYjE3OmFjZGE5MzdmLTZiOTUtNDc4OS1hMWM4LTYxM2JlNDExN2MyYg=='
-                                 })
+        response = requests.get("https://sandbox-api.marqeta.com/v3/cards/" + self.card_token,
+                                headers={
+                                    'content-type': 'application/json',
+                                    'accept': 'application/json',
+                                    'Authorization': 'Basic MThkYTc0OTYtYmU1Ny00ZDM2LWFiYjUtMDBmNTNkZDhiYjE3OmFjZGE5MzdmLTZiOTUtNDc4OS1hMWM4LTYxM2JlNDExN2MyYg=='
+                                })
         assert response.status_code == 200
         return self
 
+    @allure.step("Create a transaction by adding funds into card using card token")
     def create_transaction(self):
         data = '{"amount": "10", "mid": "123456890","card_token": "' + self.card_token + '"} '
         response = requests.post("https://sandbox-api.marqeta.com/v3/simulate/authorization",
--- tests/test_transaction_creation.py
+++ tests/test_transaction_creation.py
@@ -1,13 +1,15 @@
+import allure
 from src.transaction_base import TransactionBase
 
 
 class TestCreateTransaction(object):
 
+    @allure.title("Marqeta API Test")
     def test_create_transaction(self):
         tb = TransactionBase()
         tb.create_user() \
-            .retrive_user()\
-            .get_card_product()\
-            .create_card()\
+            .retrive_user() \
+            .get_card_product() \
+            .create_card() \
             .retrieve_card() \
             .create_transaction()
